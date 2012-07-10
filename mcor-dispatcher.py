a6529d465f875da75d4016c0976467761686d8c7
bzztbomb <skinny@knowhere.net>
2012-07-10 10:20:56 -0700

    Some sketching of the dispatcher.. untested.

--- mcor-dispatcher.py
+++ mcor-dispatcher.py
@@ -3,6 +3,10 @@ import select
 import socket
 from struct import *
 
+def usage():
+	print "Usage:"
+	print " mcor-disatcher <port> (2084 of course bitz)";
+
 def filter_space(value):
     if (value == 0x3A):
         return ' '
@@ -22,7 +26,11 @@ def get_score(values, offset):
     b = b + bcd_byte(values[offset+3], 1)
     return b
 
-def parse_scoreboard():
+def parse_scoreboard(msg):
+    # TODO: msg contains the initials and score of the latest player, rename last_death.gif to <initials>_<score>.gif
+    # This may take a little more thought, we may need to avoid collisions of people with the same initials and score repeating?  Maybe we don't
+    # care though, it's nice and stateless this way.
+
     f = open("/Users/bzztbomb/projects/churchOfRobotron/mame_146/nvram/robotron/nvram", "r")
     # Combine nibbles in values array
     values = []
@@ -31,6 +39,8 @@ def parse_scoreboard():
         b = unpack("BB", byte)
         values.append(((b[0] & 0xF) << 4) | (b[1] & 0xF))
         byte = f.read(2)
+
+    # TODO: Spit out a scores.txt that contains csv of initials, score, and <initials>_<score>.gif
 
     # GC is stored a bit funny because you can optionally enter a 20 digit initial
     print get_initials(values, 0x99)
@@ -43,6 +53,10 @@ def parse_scoreboard():
         print get_score(values, offset+3)
         offset = offset + 7
     f.close()
+
+# We should save each death face to last_death.gif
+def save_player_face():
+    pass
 
 dump_hex = lambda x: " ".join([hex(ord(c))[2:].zfill(2) for c in x])
 
@@ -57,6 +71,8 @@ def main(argv=None):
 
    port = int(argv[1])
 
+   gamerunning = False
+
    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
    print "Opening port %d" %(port)
    s.bind(('127.0.0.1', port))
@@ -68,8 +84,14 @@ def main(argv=None):
       msg = result[0][0].recv(80) # 10 bytes
       print "%s | %s" %(dump_hex(msg), msg)
 
+      if (msg.startswith("Game start")):
+          gamerunning = True
+      if (msg.startswith("Game over")):
+          gamerunning = False
+      if (msg.startswith("Player death")):
+          save_player_face()
       if (msg.startswith("NewScores")):
-          parse_scoreboard()
+          parse_scoreboard(msg)
 
 if __name__ == "__main__":
    sys.exit(main())
