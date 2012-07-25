76f890972147c208cb1213fdd209158c895c7d85
Karl Anderson <kra@monkey.org>
2012-07-24 21:45:24 -0700

    organize the flicker patter

--- effects/flicker/flicker.ino
+++ effects/flicker/flicker.ino
@@ -3,46 +3,52 @@
 
 const int len = 4;
 Metro metros[len] = {
-  Metro(10, true), Metro(10, true), Metro(10, true), Metro(10, true)};
+  Metro(1, true), Metro(10, true), Metro(100, true), Metro(1000, true)};
 Queue queues[len] = {Queue(), Queue(), Queue(), Queue()};
-int leds[len] = {12, 10, 9, 8};
+int pins[len] = {9, 10, 12, 13};
 int states[len] = {LOW, LOW, LOW, LOW};
 
-float msbase = 100;
-float lenbase = 1;
-float gapbase = 10000;
+static float msbase = 100;
+static float msmax = 1750;
+static float lenbase = 1;
+static float lenmax = 6;
+static float gapbase = 20000;
+static float gapmin = 500;
+static float offmin = 10;
+static float offmax = 250;
 
 void loadQueue(Queue *queue, int ms, int len, int gap)
 {
-  len = random(1, min(len, 6));
-  ms = min(ms, 3000);
-  gap = max(100, random(gap / 2, gap));
-  // push delay between group
-  queue->push(0);
-  queue->push(gap);
-  // push even-parity group of flicker ons and offs
+  len = random(1, len);
+  gap = random(gap / 2, gap);
+  // push even parity group of ons and offs
   for (int i = 0; i < len; i++) {
-    queue->push(random(10, min(ms, 2000)));
-    queue->push(random(10, 250));
+    queue->push(random(1, ms));
+    queue->push(random(offmin, offmax));
   }
+  // push a final on to make us odd parity
+  queue->push(random(1, ms));
+  // push delay between group to make us even parity
+  queue->push(gap);
+
 }
 
-void toggleLed(int led)
+void togglePin(int pin)
 {
-  if (states[led] == HIGH)  { 
-    states[led] = LOW;
+  if (states[pin] == HIGH)  { 
+    states[pin] = LOW;
   } 
   else {
-    states[led] = HIGH;
+    states[pin] = HIGH;
   }
-  digitalWrite(leds[led], states[led]);
+  digitalWrite(pins[pin], states[pin]);
 }
 
 void setup()
 {
   for (int i = 0; i < len; i++) {
-    pinMode(leds[i], OUTPUT);
-    digitalWrite(leds[i], states[i]);
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], states[i]);
   }
 }
 
@@ -50,13 +56,22 @@ void loop()
 {
   for (int i = 0; i < len; i++) {
     if (queues[i].isEmpty()) {
+      // We've used up this sequence of firings, speed up and load another.
+      msbase = min(msbase * 1.05, msmax);
+      lenbase = min(lenbase * 1.05, lenmax);
+      gapbase = max(gapbase * 0.97, gapmin);
       loadQueue(&queues[i], msbase, lenbase, gapbase);
-      msbase = msbase * 1.1;
-      gapbase = gapbase * 0.95;
-      lenbase = lenbase * 1.1;
+      // write a diagnostic
+      Serial.println("");
+      Serial.println("loop");
+      Serial.println(msbase);
+      Serial.println(lenbase);
+      Serial.println(gapbase);
     }
     if (metros[i].check() == 1) {
-      toggleLed(i);
+      // the metro has fired.  Toggle its pin and tell it to fire when
+      // the pin should be toggled again.
+      togglePin(i);
       metros[i].interval(queues[i].pop());
     }
   }    
