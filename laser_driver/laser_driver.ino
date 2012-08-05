5731e112f8e56235f0a1b22ed2e3f0c28be65f9b
Philip Odom <podom@pure-imagination.com>
2012-08-05 12:03:55 -0700

    added laser_pulse command: send "LAS1:G" for random, .5 second laser pulse

--- laser_driver/laser_driver.ino
+++ laser_driver/laser_driver.ino
@@ -16,15 +16,101 @@ const int led_off = LOW;
 
 #define MY_TYPE "LAS"
 #define MY_ADDR "1"
+#define BEAT_TYPE "BEAT"
+#define BEAT_ADDR "1"
 #define SEPARATOR ":"
 String preamble=MY_TYPE MY_ADDR SEPARATOR;
+String beat_pre=BEAT_TYPE BEAT_ADDR SEPARATOR;
 String command;
 
-//#define DEBUG
+#define DEBUG
 
 const unsigned int num_lasers=10;
 
 const unsigned int baud=9600;
+
+class laser
+{
+  public:
+  laser(unsigned int pin_idx):
+  _pin(pin_idx),
+  _state(false),
+  _pulse_mode(false),
+  _off_time(0)
+  {
+  }
+
+  ~laser() {
+    _off();
+    pinMode(_pin, INPUT);
+  }
+
+  void init() {
+    pinMode(_pin, OUTPUT);
+    _off();
+  }
+
+  void tick () {
+    unsigned long time=millis();
+    if (_state && ((_pulse_mode && _off_time<=time) || _beat_off<=time))
+    {
+      _off();
+    }
+  }
+
+  void heartbeat() {
+    _beat_off=millis()+_beat_timeout;
+  }
+
+  void pulse()
+  {
+    if (!_state) {
+      _on();
+      _pulse_mode=true;
+      _off_time=millis()+_pulse_time;
+    } else {
+      _off();
+    }
+    heartbeat();
+  }
+
+  void steady()
+  {
+    _on();
+    _pulse_mode=false;
+    heartbeat();
+  }
+
+  void off()
+  {
+    _off();
+    heartbeat();
+  }
+
+  private:
+  unsigned int _pin;
+  bool _state;
+  bool _pulse_mode;
+  unsigned long _off_time;
+  static unsigned long _beat_off; //;-)
+  static const unsigned long _beat_timeout=10000;
+  static const unsigned int _pulse_time=500;
+
+  void _off() {
+    digitalWrite(_pin, LOW);
+    _state=false;
+  }
+
+  void _on() {
+    digitalWrite(_pin, HIGH);
+    _state=true;
+  }
+};
+  
+unsigned long laser::_beat_off=0;  
+
+laser laser0(0), laser1(1), laser2(2), laser3(3), laser4(4), laser5(5), laser6(6), laser7(7), laser8(8), laser9(9);
+laser *lasers[num_lasers]={&laser0,&laser1,&laser2,&laser3,&laser4,&laser5,&laser6,&laser7,&laser8,&laser9};
 
 void setup()
 {
@@ -33,47 +119,74 @@ void setup()
 	digitalWrite(reset_pin, HIGH);
 	pinMode(reset_pin, OUTPUT);
 	Serial.begin(baud);	// USB, communication to PC or Mac
-  for (int i=num_lasers; i!=0;)
-    {
-    --i;
-    pinMode(i,OUTPUT);
-    digitalWrite(i,LOW);
-    }
+  for (unsigned int i=0; i != num_lasers ; ++i)
+  {
+    lasers[i]->init();
+  }
 }
 
 void set_lasers(unsigned int mask)
   {
-  for (int i=0; i<num_lasers; ++i)
+  for (unsigned int i=0; i<num_lasers; ++i)
     {
-    digitalWrite(i,(mask & 1) != 0 ? HIGH : LOW);
+    if ((mask & 1u) != 0u){
+      lasers[i]->steady();
+    } else {
+      lasers[i]->off();
+    }
     mask>>=1;
     }
   }
 
-long led_on_time=0;
+void random_laser_pulse() {
+  static unsigned int last_laser=0;
+  unsigned int next_laser=random(0,num_lasers-1);
+  if (next_laser==last_laser) {
+    next_laser++;
+  }
+  lasers[next_laser]->pulse();
+#ifdef debug
+  Serial.println ("random pulse");
+  Serial.println (next_laser,HEX);
+#endif
+  last_laser=next_laser;
+}
 
 void parse_command(const String &cmd)
-  {
+{
   //command format: <my_type><my_addr>SEPARATOR<
   //check for command preamble
-  if (!cmd.startsWith(preamble))
-    {
-    Serial.println("bad preamble");
-    return;
+  if (cmd.startsWith(beat_pre))
+  {
+    for (unsigned int i=0; i!=0 ; ++i) {
+      lasers[i]->heartbeat();
     }
-  
-  String num=cmd.substring(preamble.length(),preamble.length()+4);
-  
-  char buf[4];
-  num.toCharArray(buf,4); //up to 4 characters
-  unsigned int mask;
-  sscanf(buf,"%x",&mask);
-  set_lasers(mask);
 #ifdef DEBUG
-  Serial.println("good command");
-  Serial.println(mask,HEX);
+    Serial.println("good heartbeat");
+#endif
+  } else if (cmd.startsWith(preamble)) {
+    String num=cmd.substring(preamble.length(),preamble.length()+4);
+    if (num.startsWith('g') || num.startsWith('G')) {
+      random_laser_pulse();
+#ifdef DEBUG
+      Serial.println("good pulse");
+#endif
+    } else {
+      char buf[4];
+      num.toCharArray(buf,4); //up to 4 characters
+      unsigned int mask;
+      sscanf(buf,"%x",&mask);
+      set_lasers(mask);
+#ifdef DEBUG
+      Serial.println("good mask");
+#endif
+    }
+  } else {
+#ifdef debug
+    Serial.println("bad preamble");
 #endif
   }
+}
 
 void loop()
 {
@@ -97,5 +210,8 @@ void loop()
     else
       {command+=c;}
     }
+  for (unsigned int i=0; i!=num_lasers; ++i) {
+    lasers[i]->tick();
+  }
 }
 
